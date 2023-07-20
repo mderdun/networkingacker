0330f68e2ba32ddbf11d294ddeae26d360bd7e8e
mderdun <mderdun@gmail.com>
2023-07-20 14:33:45 -0400

    Updated About page

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -1,1 +1,4 @@
-<h1>Dissertation</h1>
+<h1>About</h1>
+<p>Exploring Kathy Acker's networks.</p>
+<h2>Phase 1 (Undergrad Dissertation)</h2>
+<p>Extracting, reconstructing, visualising, and analysing networks of narrative and identity in the fiction of Kathy Acker.</p>

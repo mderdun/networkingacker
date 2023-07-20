845f7eb72852a383d0bd9fa32c0cd6574a4f9150
mderdun <mderdun@gmail.com>
2023-07-20 15:40:40 -0400

    Revert "Configured for GithubPages"
    This reverts commit d19b9c094537ead2b867fed94c6d150a98c017f7.

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from '@sveltejs/adapter-static';
+import adapter from '@sveltejs/adapter-vercel';
 import { vitePreprocess } from '@sveltejs/kit/vite';
 
 import { escapeSvelte, mdsvex } from 'mdsvex';
@@ -6,8 +6,6 @@ import shiki from 'shiki';
 import remarkUnwrapIages from 'remark-unwrap-images';
 import remarkToc from 'remark-toc';
 import rehypeSlug from 'rehype-slug';
-
-const dev = "production" === "development";
 
 /** @type {import('mdsvex').mdsvexOptions} */
 const mdsvexOptions = {
@@ -29,16 +27,7 @@ const config = {
 	preprocess: [vitePreprocess(), mdsvex(mdsvexOptions)],
 
 	kit: {
-		adapter: adapter({
-            pages: "docs",
-            assets: "docs"
-        }),
-        paths: {
-            // change below to your repo name
-            base: dev ? "" : "/networkingacker",
-        },
-        // hydrate the <div id="svelte"> element in src/app.html
-        target: "#svelte",
+		adapter: adapter(),
 		prerender: {
 			handleHttpError: async ({ request, resolve }) => {
 				const response = await resolve(request);

b5618a295d32b302667a504d568c2441fb6d71fe
mderdun <mderdun@gmail.com>
2023-07-23 00:52:02 -0400

    Dissertation WIP display

---
+++ src/components/WIP.svelte
@@ -0,0 +1,43 @@
+<div class="police-tape--1">
+	<h1 class="police-tape-text">
+		&nbsp;&nbsp;&nbsp;&nbsp;Under Construction&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Under
+		Construction&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Under
+		Construction&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Under
+		Construction&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Under
+		Construction&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Under
+		Construction&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Under Construction
+	</h1>
+</div>
+<div class="police-tape--2">
+	<h1 class="police-tape-text">
+		Forbidden&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Forbidden&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Forbidden&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Forbidden&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Forbidden&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Forbidden&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+	</h1>
+</div>
+
+<style>
+	.police-tape-text {
+		padding: 0.125em;
+		font-size: 3em;
+		text-align: center;
+		white-space: nowrap;
+	}
+
+	.police-tape--1 {
+		background-color: var(--yellow-3);
+		transform: rotate(10deg);
+		position: absolute;
+		top: 40%;
+		left: -5%;
+		right: -5%;
+		z-index: 2;
+		margin-top: 0;
+	}
+	.police-tape--2 {
+		background-color: var(--yellow-3);
+		transform: rotate(-8deg);
+		position: absolute;
+		top: 50%;
+		left: -5%;
+		right: -5%;
+	}
+</style>
--- src/routes/dissertation/+page.svelte
+++ src/routes/dissertation/+page.svelte
@@ -1,1 +1,33 @@
-<h1>About</h1>
+<script>
+    import WIP from "../../components/WIP.svelte";
+</script>
+
+<h1>Dissertation</h1>
+<p>Bear with me I'm still writing it...</p>
+<p>Once it's done, I'll upload an interactive web version here.</p>
+<pre>
+    d8b                                 d8b                                      
+    d8P   ?88                           ?88                                      
+ d888888P  88b                           88b                                     
+   ?88'    888888b  d888b8b    88bd88b   888  d88'    ?88   d8P  d8888b ?88   d8P
+   88P     88P `?8bd8P' ?88    88P' ?8b  888bd8P'     d88   88  d8P' ?88d88   88 
+   88b    d88   88P88b  ,88b  d88   88P d88888b       ?81  d88  88b  d88?81  d88 
+   `?8b  d88'   88b`?88P'`88bd88'   88bd88' `?88b,    `?88P'?8b `?8888P'`?88P'?8b
+                                                             188                 
+                                                            ,d8P                 
+                                                         `?888P'                 
+                                            
+</pre>
+
+<div>
+    <WIP />
+</div>
+
+<style>
+    pre {
+        margin-top: 24rem;
+        font-family: monospace;
+        font-size: 0.5em;
+        color: var(--red-8);
+    }
+</style>
